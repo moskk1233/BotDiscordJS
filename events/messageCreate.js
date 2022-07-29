c490329ea0f64a2d5ed5e5a61341e33c5f7f1073
Nonpawit Pothinil <54355463+moskk1233@users.noreply.github.com>
2022-07-29 11:28:38 +0700

    Remove messageCreate.js
    messageCreate.js เป็นชุดคำสั่งที่ตรวจสอบหาข้อความแล้วเขียนส่งข้อความแทนด้วยบอท

--- events/messageCreate.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name : "messageCreate",
-    async execute(message) {
-        if (message.author.bot) return
-        message.delete()
-        await message.channel.send(`**${message.author.tag}**: ${message.content}`)
-    }
-}
