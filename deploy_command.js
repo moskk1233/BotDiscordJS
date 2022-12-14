3e4efebc301b8b05617278be267765d2d23d2174
moskk1233 <mosaggpothinil@gmail.com>
2022-12-14 13:30:30 +0700

    deploy: เปลี่ยนตารางการdeployคำสั่ง

--- deploy_command.js
+++ deploy_command.js
@@ -1,25 +1,30 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
 const { REST } = require("@discordjs/rest")
-const { Routes } = require("discord-api-types/v10")
-const fs = require("node:fs")
-const path = require("node:path")
-require("dotenv").config()
+const { Routes } = require("discord-api-types/v10");
+const fs = require("node:fs");
+const path = require("node:path");
+require("dotenv").config();
+const ascii = require("ascii-table");
+let table = new ascii("Commands")
+    .setHeading("Command", "Deploy Status");
 
 
-const commands = []
-const commandPath = path.join(__dirname, "command_handler")
-const commandFiles = fs.readdirSync(commandPath).filter(file => file.endsWith(".js"))
+const commands = [];
+const commandPath = path.join(__dirname, "command_handler");
+const commandFiles = fs.readdirSync(commandPath).filter(file => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-    const filePath = path.join(commandPath, file)
-    const slashcommand = require(filePath)
-    commands.push(slashcommand.data.toJSON())
+    const filePath = path.join(commandPath, file);
+    const slashcommand = require(filePath);
+    try {
+        commands.push(slashcommand.data.toJSON());
+        table.addRow(file, "✅ All Pass!!");
+    }
+    catch {
+        table.addRow(file, "❌ --> Insert data.SlashCommandBuilder in This file");
+    }
 }
 
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN)
-console.log(`+-------------------+---+\n| Command Uploading |   |\n+-------------------+---+`)
-rest.put(Routes.applicationCommands(process.env.CLIENTID), { body: commands })
-.then(() => {
-    console.log(`+-------------------+---+\n| Command Uploading | ✓ |\n+-------------------+---+\n`)
-})
-.catch(console.error)
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+rest.put(Routes.applicationCommands(process.env.CLIENTID), { body: commands });
+console.log(table.toString())
